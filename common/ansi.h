3f05e41079af00cb685840b83ad0fc9908a4666b
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2024-10-31 17:02:11 +0600

    Added ansi style header

---
+++ common/ansi.h
@@ -0,0 +1,57 @@
+#ifndef COMMON_ANSI_H_
+#define COMMON_ANSI_H_
+
+
+#define ANSI_RESET          "\033[0m"
+
+/* Text styles */
+#define ANSI_BOLD           "\033[1m"
+#define ANSI_DIM            "\033[2m"
+#define ANSI_ITALIC         "\033[3m"
+#define ANSI_UNDERLINE      "\033[4m"
+#define ANSI_BLINK          "\033[5m"
+#define ANSI_INVERSE        "\033[7m"
+#define ANSI_HIDDEN         "\033[8m"
+#define ANSI_STRIKETHROUGH  "\033[9m"
+
+/* Foreground colors */
+#define ANSI_FG_DEFAULT     "\033[39m"
+#define ANSI_FG_BLACK       "\033[30m"
+#define ANSI_FG_RED         "\033[31m"
+#define ANSI_FG_GREEN       "\033[32m"
+#define ANSI_FG_YELLOW      "\033[33m"
+#define ANSI_FG_BLUE        "\033[34m"
+#define ANSI_FG_MAGENTA     "\033[35m"
+#define ANSI_FG_CYAN        "\033[36m"
+#define ANSI_FG_WHITE       "\033[37m"
+#define ANSI_FG_BRIGHT_BLACK "\033[90m"
+#define ANSI_FG_BRIGHT_RED  "\033[91m"
+#define ANSI_FG_BRIGHT_GREEN "\033[92m"
+#define ANSI_FG_BRIGHT_YELLOW "\033[93m"
+#define ANSI_FG_BRIGHT_BLUE "\033[94m"
+#define ANSI_FG_BRIGHT_MAGENTA "\033[95m"
+#define ANSI_FG_BRIGHT_CYAN "\033[96m"
+#define ANSI_FG_BRIGHT_WHITE "\033[97m"
+
+/* Background colors */
+#define ANSI_BG_DEFAULT     "\033[49m"
+#define ANSI_BG_BLACK       "\033[40m"
+#define ANSI_BG_RED         "\033[41m"
+#define ANSI_BG_GREEN       "\033[42m"
+#define ANSI_BG_YELLOW      "\033[43m"
+#define ANSI_BG_BLUE        "\033[44m"
+#define ANSI_BG_MAGENTA     "\033[45m"
+#define ANSI_BG_CYAN        "\033[46m"
+#define ANSI_BG_WHITE       "\033[47m"
+#define ANSI_BG_BRIGHT_BLACK "\033[100m"
+#define ANSI_BG_BRIGHT_RED  "\033[101m"
+#define ANSI_BG_BRIGHT_GREEN "\033[102m"
+#define ANSI_BG_BRIGHT_YELLOW "\033[103m"
+#define ANSI_BG_BRIGHT_BLUE "\033[104m"
+#define ANSI_BG_BRIGHT_MAGENTA "\033[105m"
+#define ANSI_BG_BRIGHT_CYAN "\033[106m"
+#define ANSI_BG_BRIGHT_WHITE "\033[107m"
+
+
+#endif /* !COMMON_ANSI_H_ */
+
